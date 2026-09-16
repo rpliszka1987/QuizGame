b3fd067e6f761c8b7d9c97f77f5c916fb6de27be
Robert Pliszka <robplisz@gmail.com>
2026-09-15 21:04:15 -0400

    Created QuizBrain class which created new quiz object nad ask first question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from question_model import Question
+from quiz_brain import QuizBrain
 from data import question_data
 
 question_bank = []
@@ -9,4 +10,5 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
